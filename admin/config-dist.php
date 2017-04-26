4537c83d3903ee292614718853113ae43bb560d4
Rich <rich@five-star.com>
2017-04-26 11:04:39 -0400

    After install... install directory moved to install.old and sql file dumped to it.

---
+++ admin/config-dist.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://netbundle.five-star.com/admin/');
+define('HTTP_CATALOG', 'http://netbundle.five-star.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://netbundle.five-star.com/admin/');
+define('HTTPS_CATALOG', 'http://netbundle.five-star.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home1/fivesta4/public_html/netbundle/admin/');
+define('DIR_SYSTEM', '/home1/fivesta4/public_html/netbundle/system/');
+define('DIR_IMAGE', '/home1/fivesta4/public_html/netbundle/image/');
+define('DIR_LANGUAGE', '/home1/fivesta4/public_html/netbundle/admin/language/');
+define('DIR_TEMPLATE', '/home1/fivesta4/public_html/netbundle/admin/view/template/');
+define('DIR_CONFIG', '/home1/fivesta4/public_html/netbundle/system/config/');
+define('DIR_CACHE', '/home1/fivesta4/public_html/netbundle/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home1/fivesta4/public_html/netbundle/system/storage/download/');
+define('DIR_LOGS', '/home1/fivesta4/public_html/netbundle/system/storage/logs/');
+define('DIR_MODIFICATION', '/home1/fivesta4/public_html/netbundle/system/storage/modification/');
+define('DIR_UPLOAD', '/home1/fivesta4/public_html/netbundle/system/storage/upload/');
+define('DIR_CATALOG', '/home1/fivesta4/public_html/netbundle/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', '');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'nb_');
---
+++ config-dist.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://netbundle.five-star.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://netbundle.five-star.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home1/fivesta4/public_html/netbundle/catalog/');
+define('DIR_SYSTEM', '/home1/fivesta4/public_html/netbundle/system/');
+define('DIR_IMAGE', '/home1/fivesta4/public_html/netbundle/image/');
+define('DIR_LANGUAGE', '/home1/fivesta4/public_html/netbundle/catalog/language/');
+define('DIR_TEMPLATE', '/home1/fivesta4/public_html/netbundle/catalog/view/theme/');
+define('DIR_CONFIG', '/home1/fivesta4/public_html/netbundle/system/config/');
+define('DIR_CACHE', '/home1/fivesta4/public_html/netbundle/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home1/fivesta4/public_html/netbundle/system/storage/download/');
+define('DIR_LOGS', '/home1/fivesta4/public_html/netbundle/system/storage/logs/');
+define('DIR_MODIFICATION', '/home1/fivesta4/public_html/netbundle/system/storage/modification/');
+define('DIR_UPLOAD', '/home1/fivesta4/public_html/netbundle/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', '');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'nb_');
